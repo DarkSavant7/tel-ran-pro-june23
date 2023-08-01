caea43e61b3a47d4074593736d47e595fb93687f
Alex Grigorev <persian89g@gmail.com>
2023-08-01 16:38:48 +0400

    start bot

---
+++ telegram-bot-example/src/main/java/de/tel/ran/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package de.tel.ran.repository;
+
+import de.tel.ran.domain.User;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public interface UserRepository {
+
+  User create(User user);
+  void deleteByTelegramId(String telegramId);
+  User update(User user);
+  Integer incrementCount(String telegramId);
+  LocalDateTime getLastSmokeTime(String telegramId);
+  User getNearestUserForNotification();
+  Set<User> getUsersByNotificationPeriod(LocalDateTime from, LocalDateTime to);
+}
