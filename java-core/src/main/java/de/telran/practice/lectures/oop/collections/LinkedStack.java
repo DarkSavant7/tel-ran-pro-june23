68e25861f006093f78b9299c47db0ac9819e45d9
Alex Grigorev <persian89g@gmail.com>
2023-08-07 15:16:30 +0400

    add linked stack dummy

---
+++ java-core/src/main/java/de/telran/practice/lectures/oop/collections/LinkedStack.java
@@ -0,0 +1,22 @@
+package de.telran.practice.lectures.oop.collections;
+
+import java.util.Stack;
+
+public class LinkedStack {
+
+  private Node first;
+
+
+  public void push(String element) {
+
+  }
+
+  public String pop() {
+    return null;
+  }
+
+  static class Node {
+    String element;
+    Node next;
+  }
+}
