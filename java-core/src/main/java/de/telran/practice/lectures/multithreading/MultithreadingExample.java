352cd306e7b41891db6ce0a83eaf9cd841474a6a
Alex Grigorev <persian89g@gmail.com>
2023-10-04 15:13:35 +0400

    add mt task

---
+++ java-core/src/main/java/de/telran/practice/lectures/multithreading/MultithreadingExample.java
@@ -0,0 +1,16 @@
+package de.telran.practice.lectures.multithreading;
+
+public class MultithreadingExample {
+
+  /**
+   * Создать массив float c большим размером, например 100_000_000
+   * заполнить массив единицами
+   * и прогнать каждый элемент массива через обработку:
+   * элемент это arr[i], i - это индекс элемента в массиве
+   * arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5.0) * Math.cos(0.2f + i / 5.0) * Math.cos(0.4f + i / 2.0))
+   */
+
+  public static void main(String[] args) {
+
+  }
+}
